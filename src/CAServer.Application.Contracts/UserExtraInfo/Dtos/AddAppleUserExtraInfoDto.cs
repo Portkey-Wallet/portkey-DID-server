322018b4cbc9d6049073dfef9aff68e25d591c84
felix-zhaolei <felix@portkey.finance>
2023-04-19 18:17:46 +0800

    fix: remove apple login email validate.

--- src/CAServer.Application.Contracts/UserExtraInfo/Dtos/AddAppleUserExtraInfoDto.cs
+++ src/CAServer.Application.Contracts/UserExtraInfo/Dtos/AddAppleUserExtraInfoDto.cs
@@ -11,11 +11,11 @@ public class AddAppleUserExtraInfoDto
 public class AppleUserInfoDto
 {
     [Required] public AppleUserName Name { get; set; }
-    [Required] public string Email { get; set; }
+    public string Email { get; set; }
 }
 
 public class AppleUserName
 {
-    [Required] public string FirstName { get; set; }
-    [Required] public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
