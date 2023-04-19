71b519ed7be55a9c977e42fc5391a78256e2cf8a
felix-zhaolei <felix@portkey.finance>
2023-04-19 15:14:50 +0800

    feat: 1.optimize third-party login logic. 2.optimize on/off-ramp logic.

--- test/CAServer.Application.Tests/CAAccount/RegisterServiceTests.cs
+++ test/CAServer.Application.Tests/CAAccount/RegisterServiceTests.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CAServer.CAAccount.Dtos;
 using CAServer.Dtos;
 using CAServer.Grain.Tests;
 using CAServer.Grains.Grain.Guardian;
-using Microsoft.Extensions.DependencyInjection;
 using Orleans.TestingHost;
 using Shouldly;
 using Volo.Abp.Validation;
@@ -34,26 +32,6 @@ public class RegisterServiceTests : CAServerApplicationTestBase
         _caAccountAppService = GetService<ICAAccountAppService>();
         _cluster = GetRequiredService<ClusterFixture>().Cluster;
     }
-
-    private bool VerifyEmail(string address)
-    {
-        // string emailRegex =
-        //     @"([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,5})+";
-
-        var emailRegex = @"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
-        var emailReg = new Regex(emailRegex);
-        return emailReg.IsMatch(address.Trim());
-    }
-
-    [Fact]
-    public void TestEmailRegex()
-    {
-        string email = "b@b.b";
-        var res = VerifyEmail(email);
-        Assert.True(res);
-    }
-
-    #region RegisterRequestAsync
 
     [Fact]
     public async Task RegisterRequestAsync_Register_Success_Test()
@@ -87,51 +65,6 @@ public class RegisterServiceTests : CAServerApplicationTestBase
     }
 
     [Fact]
-    public async Task RegisterRequestAsync_Register_Body_Empty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex != null);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_Type_LoginGuardianType_Not_Match_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = GuardianIdentifierType.Phone,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = DefaultExtraData,
-                ChainId = DefaultChainId,
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(true);
-            // List<string> errMessageList = new List<string>() { "Invalid phone input.", "Invalid email input." };
-            // Assert.Contains(errMessageList, t => t.Contains(ex.Message));
-        }
-    }
-
-    [Fact]
     public async Task RegisterRequestAsync_Register_LoginGuardianIdentifier_Is_NullOrEmpty_Test()
     {
         try
@@ -158,175 +91,4 @@ public class RegisterServiceTests : CAServerApplicationTestBase
             Assert.True(ex is AbpValidationException);
         }
     }
-
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_Address_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = "",
-                ExtraData = DefaultExtraData,
-                ChainId = DefaultChainId,
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_DeviceString_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = "",
-                ChainId = DefaultChainId,
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_ChainId_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = DefaultExtraData,
-                ChainId = "",
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_VerifierId_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = DefaultExtraData,
-                ChainId = DefaultChainId,
-                VerifierId = "",
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_VerificationDoc_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = DefaultExtraData,
-                ChainId = DefaultChainId,
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = "",
-                Signature = DefaultVerifierSignature,
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    [Fact]
-    public async Task RegisterRequestAsync_Register_Signature_Is_NullOrEmpty_Test()
-    {
-        try
-        {
-            await _caAccountAppService.RegisterRequestAsync(new RegisterRequestDto
-            {
-                Type = 0,
-                LoginGuardianIdentifier = DefaultEmailAddress,
-                Manager = DefaultManager,
-                ExtraData = DefaultExtraData,
-                ChainId = DefaultChainId,
-                VerifierId = DefaultVerifierId,
-                VerificationDoc = DefaultVerificationDoc,
-                Signature = "",
-                Context = new HubRequestContextDto
-                {
-                    ClientId = DefaultClientId,
-                    RequestId = DefaultRequestId
-                }
-            });
-        }
-        catch (Exception ex)
-        {
-            Assert.True(ex is AbpValidationException);
-        }
-    }
-
-    #endregion
 }
--- test/CAServer.Application.Tests/Notify/NotifyTest.cs
+++ test/CAServer.Application.Tests/Notify/NotifyTest.cs
@@ -39,28 +39,9 @@ public class NotifyTest : CAServerApplicationTestBase
             IsApproved = true
         };
 
-        var result = await _notifyAppService.CreateAsync(dto);
-        result.ShouldNotBeNull();
-        result.AppId.ShouldBe(dto.AppId);
-    }
-
-    [Fact]
-    public async Task Update_Test()
-    {
-        var dto = new CreateNotifyDto
-        {
-            Title = "Update Portkey",
-            Content = "Update Portkey",
-            AppId = "100001",
-            TargetVersion = "1.2.2",
-            AppVersions = new[] { "1.0.0", "1.1.0" },
-            DeviceTypes = new[] { DeviceType.Android },
-            SendTypes = new[] { NotifySendType.None },
-            ReleaseTime = DateTime.UtcNow,
-            DownloadUrl = "http://127.0.0.1",
-            IsForceUpdate = true,
-            IsApproved = true
-        };
+        var createResult = await _notifyAppService.CreateAsync(dto);
+        createResult.ShouldNotBeNull();
+        createResult.AppId.ShouldBe(dto.AppId);
 
         var newTitle = "Update Portkey New";
         var updateDto = new UpdateNotifyDto
@@ -77,39 +58,10 @@ public class NotifyTest : CAServerApplicationTestBase
             IsForceUpdate = true,
             IsApproved = true
         };
+        var updateResult = await _notifyAppService.UpdateAsync(createResult.Id, updateDto);
 
-        var createDto = await _notifyAppService.CreateAsync(dto);
-        var result = await _notifyAppService.UpdateAsync(createDto.Id, updateDto);
-
-        result.ShouldNotBeNull();
-        result.Title.ShouldBe(newTitle);
-    }
-
-    [Fact]
-    public async Task Update_Not_Exist_Test()
-    {
-        try
-        {
-            var updateDto = new UpdateNotifyDto
-            {
-                Title = "Update Portkey",
-                Content = "Update Portkey",
-                AppId = "100001",
-                TargetVersion = "1.2.2",
-                AppVersions = new[] { "1.0.0", "1.1.0" },
-                DeviceTypes = new[] { DeviceType.Android },
-                SendTypes = new[] { NotifySendType.None },
-                ReleaseTime = DateTime.UtcNow,
-                DownloadUrl = "http://127.0.0.1",
-                IsForceUpdate = true,
-                IsApproved = true
-            };
-            await _notifyAppService.UpdateAsync(Guid.Empty, updateDto);
-        }
-        catch (Exception e)
-        {
-            e.Message.ShouldBe(NotifyMessage.NotExistMessage);
-        }
+        updateResult.ShouldNotBeNull();
+        updateResult.Title.ShouldBe(newTitle);
     }
 
     [Fact]
@@ -133,20 +85,7 @@ public class NotifyTest : CAServerApplicationTestBase
         var createDto = await _notifyAppService.CreateAsync(dto);
         await _notifyAppService.DeleteAsync(createDto.Id);
     }
-
-    [Fact]
-    public async Task Delete_Not_Exist_Test()
-    {
-        try
-        {
-            await _notifyAppService.DeleteAsync(Guid.NewGuid());
-        }
-        catch (Exception e)
-        {
-            e.Message.ShouldBe(NotifyMessage.NotExistMessage);
-        }
-    }
-
+    
     [Fact]
     public async Task Pull_Test()
     {
@@ -166,7 +105,7 @@ public class NotifyTest : CAServerApplicationTestBase
         };
 
         var createDto = await _notifyAppService.CreateAsync(dto);
-        
+
         await _notifyRulesRepository.AddOrUpdateAsync(new NotifyRulesIndex
         {
             AppId = "100001",
@@ -184,37 +123,8 @@ public class NotifyTest : CAServerApplicationTestBase
             DeviceType = DeviceType.Android,
             AppVersion = "1.0.0"
         });
-        
-        result.Title.ShouldBe("Update Portkey");
-    }
-    
-    [Fact]
-    public async Task Pull_Not_Exist_Test()
-    {
-        try
-        {
-            await _notifyRulesRepository.AddOrUpdateAsync(new NotifyRulesIndex
-            {
-                AppId = "100001",
-                AppVersions = new[] { "1.0.0", "1.1.0" },
-                DeviceTypes = new[] { "Android" },
-                SendTypes = new[] { NotifySendType.None },
-                IsApproved = true,
-                Id = Guid.NewGuid()
-            });
 
-            var result = await _notifyAppService.PullNotifyAsync(new PullNotifyDto()
-            {
-                DeviceId = "qwer",
-                AppId = "100001",
-                DeviceType = DeviceType.Android,
-                AppVersion = "1.0.0"
-            });
-        }
-        catch (Exception e)
-        {
-            e.Message.ShouldBe(NotifyMessage.NotExistMessage);
-        }
+        result.Title.ShouldBe("Update Portkey");
     }
     
 }
