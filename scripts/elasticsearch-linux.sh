f380fbac4063d3963adb5b6fbde0a0b1c59d07cc
felix-zhaolei <felix@portkey.finance>
2023-04-25 11:53:55 +0800

    modify:moidfy ci

--- scripts/elasticsearch-linux.sh
+++ scripts/elasticsearch-linux.sh
@@ -2,6 +2,7 @@
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.1-linux-x86_64.tar.gz
 tar xzvf elasticsearch-7.15.1-linux-x86_64.tar.gz
 useradd elasticsearch
+kill -s 9 `pgrep elasticsearch`
 chown -R elasticsearch:elasticsearch elasticsearch-7.15.1
 cd elasticsearch-7.15.1/
 su elasticsearch -c "bin/elasticsearch -d"
