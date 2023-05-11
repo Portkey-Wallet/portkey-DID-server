cba78732dbb194c29cd32f11913d94588b01ba4f
wangyue <sean@portkey.finance>
2023-05-11 19:30:34 +0800

    optimize CrossChainTransfer query

--- src/CAServer.ContractEventHandler.Core/Application/CrossChainTransferAppService.cs
+++ src/CAServer.ContractEventHandler.Core/Application/CrossChainTransferAppService.cs
@@ -92,8 +92,8 @@ public class CrossChainTransferAppService : ICrossChainTransferAppService, ITran
             }
 
             var indexedHeight = await _graphQlProvider.GetIndexBlockHeightAsync(chainId);
-            var startHeight = latestProcessedHeight + 1 - _indexOptions.IndexSafe;
-            var endHeight = Math.Min(startHeight + MaxTransferQueryCount - 1, indexedHeight);
+            var startHeight = latestProcessedHeight + 1;
+            var endHeight = Math.Min(startHeight + MaxTransferQueryCount - 1, indexedHeight - _indexOptions.IndexSafe);
 
             while (true)
             {
@@ -126,8 +126,8 @@ public class CrossChainTransferAppService : ICrossChainTransferAppService, ITran
                     break;
                 }
 
-                startHeight = endHeight + 1 - _indexOptions.IndexSafe;
-                endHeight = Math.Min(startHeight + MaxTransferQueryCount - 1, indexedHeight);
+                startHeight = endHeight + 1;
+                endHeight = Math.Min(startHeight + MaxTransferQueryCount - 1, indexedHeight-_indexOptions.IndexSafe);
             }
         }
 
