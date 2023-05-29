ad356efa960af0d83e175711e9a73e8e3952d196
felix-zhaolei <felix@portkey.finance>
2023-05-29 19:02:14 +0800

    feat: add ut.

--- test/CAServer.Application.Tests/Common/ModelTest.cs
+++ test/CAServer.Application.Tests/Common/ModelTest.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
-using CAServer.Contacts;
 using CAServer.Guardian;
 using CAServer.Hubs;
 using CAServer.IpInfo;
+using CAServer.Sample;
+using CAServer.ThirdPart.Dtos;
+using CAServer.Verifier;
 using Shouldly;
 using Xunit;
 
@@ -61,7 +63,151 @@ public class ModelTest
     {
         var dto = new IpInfoDto
         {
-            Ip = string.Empty
+            Ip = string.Empty,
+            Type = string.Empty,
+            ContinentCode = string.Empty,
+            ContinentName = string.Empty,
+            CountryCode = string.Empty,
+            CountryName = string.Empty,
+            RegionCode = string.Empty,
+            RegionName = string.Empty,
+            City = string.Empty,
+            Zip = string.Empty,
+            Latitude = 0,
+            Longitude = 0,
+            Location = new LocationInfo()
+            {
+                GeonameId = 1,
+                Capital = string.Empty,
+                Languages = new List<LanguageInfo>()
+                {
+                    new LanguageInfo()
+                    {
+                        Code = string.Empty,
+                        Name = string.Empty,
+                        Native = string.Empty
+                    }
+                },
+                CountryFlag = string.Empty,
+                CountryFlagEmoji = string.Empty,
+                CountryFlagEmojiUnicode = string.Empty,
+                CallingCode = string.Empty,
+                IsEu = false
+            }
         };
+
+        dto.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void TestDto()
+    {
+        var dto = new TestDto
+        {
+            From = string.Empty,
+            To = string.Empty,
+            Message = string.Empty
+        };
+
+        dto.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void ACHDtoTest()
+    {
+        var requestDto = new GetAlchemyOrderQuoteDto
+        {
+            Crypto = string.Empty,
+            Network = string.Empty,
+            Fiat = string.Empty,
+            Country = string.Empty,
+            Amount = string.Empty,
+            Side = string.Empty,
+            PayWayCode = string.Empty,
+            Type = string.Empty
+        };
+
+        var responseDto = new AlchemyOrderQuoteResultDto
+        {
+            Data = new AlchemyOrderQuoteDataDto()
+            {
+                Crypto = string.Empty,
+                CryptoPrice = string.Empty,
+                Fiat = string.Empty,
+                CryptoQuantity = string.Empty,
+                RampFee = string.Empty,
+                NetworkFee = string.Empty,
+                PayWayCode = string.Empty
+            }
+        };
+
+        var dtos = new AlchemyCryptoListDto
+        {
+            Data = new List<AlchemyCryptoDto>()
+            {
+                new AlchemyCryptoDto()
+                {
+                    Crypto = string.Empty,
+                    Network = string.Empty,
+                    BuyEnable = string.Empty,
+                    SellEnable = string.Empty,
+                    MinPurchaseAmount = string.Empty,
+                    MaxPurchaseAmount = string.Empty,
+                    Address = string.Empty,
+                    Icon = string.Empty,
+                    MinSellAmount = string.Empty,
+                    MaxSellAmount = string.Empty
+                }
+            }
+        };
+
+        var fiatListDto = new AlchemyFiatListDto
+        {
+            Data = new List<AlchemyFiatDto>()
+            {
+                new AlchemyFiatDto()
+                {
+                    Currency = string.Empty,
+                    Country = string.Empty,
+                    PayWayName = string.Empty,
+                    FixedFee = string.Empty,
+                    RateFee = string.Empty,
+                    PayMin = string.Empty,
+                    PayMax = string.Empty,
+                    PayWayCode = string.Empty
+                }
+            }
+        };
+
+        var tokenDto = new AlchemyTokenDto
+        {
+            Data = new AlchemyTokenDataDto()
+            {
+                Email = string.Empty,
+                AccessToken = string.Empty
+            }
+        };
+
+        requestDto.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void VerifierDtoTest()
+    {
+        var serverInfo = new GuardianVerifierServer
+        {
+            Name = string.Empty,
+            VerifierAddress = null,
+            ImageUrl = string.Empty,
+            EndPoints = null
+        };
+
+        var verifierServerInfo = new VerifierServerInfo
+        {
+            EndPoints = new List<string>(),
+            Id = string.Empty
+        };
+
+        serverInfo.ShouldNotBeNull();
     }
 }
