4d40e4813a34c85b8c85c8f72cab4a9c45062520
felix-zhaolei <felix@portkey.finance>
2023-05-29 10:54:41 +0800

    fix: modify apple login ut.

--- test/CAServer.Application.Tests/AppleAuth/AppleAuthTest.cs
+++ test/CAServer.Application.Tests/AppleAuth/AppleAuthTest.cs
@@ -41,11 +41,14 @@ public partial class AppleAuthTest : CAServerApplicationTestBase
                 Code = "",
                 Id_token =
                     "eyJraWQiOiJXNldjT0tCIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLnBvcnRrZXkuZGlkIiwiZXhwIjoxNjc5NDcyMjg1LCJpYXQiOjE2NzkzODU4ODUsInN1YiI6IjAwMDMwMy5jZDgxN2I2OTgzMDc0ZDhjOGZiNzkyNDk2ZjI3N2ViYy4wMjU3IiwiY19oYXNoIjoicFBSeFFTSWNWY19BTEExSE9vdmJ5QSIsImVtYWlsIjoicHQ2eXhtOXptbUBwcml2YXRlcmVsYXkuYXBwbGVpZC5jb20iLCJlbWFpbF92ZXJpZmllZCI6InRydWUiLCJpc19wcml2YXRlX2VtYWlsIjoidHJ1ZSIsImF1dGhfdGltZSI6MTY3OTM4NTg4NSwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.wXHXNbQVqvRxK_a6dq3WjBbJe_KaGsRVgSz_i3E01JyKW8rxGRRgDqjYNiTxB6iOqBMfvXfjtjgPl1N-de_Q4OflzG7gKK_17c-sY2uXUbOWVtAFI9WEXksYhZdV66eJDiUKJ8KE94S6NCT8UdkRqtxHtCnjuq82taYPbqcb-NO3Xcu23hfKsYQM_73yHJfnFd7jUYCoLHcxlVUeRGR7D7L3Yo9FdbocHZwei_x_jwb_7gYjTqGKg6rYt4MRT5ElSTj4xajXrRLZZzCFTVPjytvUsGvU038SEj4sIK6eoDAQy90ne2_XritzViMfKcWid6cdgh-Zz3PzfRx9LEyIPg",
-                User = JsonConvert.SerializeObject(new AppleUserExtraInfo
+                User = JsonConvert.SerializeObject(new AppleExtraInfo
                 {
-                    UserId = Guid.NewGuid().ToString("N"),
-                    FirstName = "Kui",
-                    LastName = "Li"
+                    Name = new AppleNameInfo()
+                    {
+                        FirstName = "Li",
+                        LastName = "Ning"
+                    },
+                    Email = "test@qq.com"
                 })
             });
         }
@@ -78,7 +81,8 @@ public partial class AppleAuthTest : CAServerApplicationTestBase
     private static ClaimsPrincipal SelectClaimsPrincipal()
     {
         IPrincipal currentPrincipal = Thread.CurrentPrincipal;
-        return currentPrincipal is ClaimsPrincipal claimsPrincipal ? claimsPrincipal : (currentPrincipal == null ? (ClaimsPrincipal)null : new ClaimsPrincipal(currentPrincipal));
+        return currentPrincipal is ClaimsPrincipal claimsPrincipal
+            ? claimsPrincipal
+            : (currentPrincipal == null ? (ClaimsPrincipal)null : new ClaimsPrincipal(currentPrincipal));
     }
-
-} 
+}
