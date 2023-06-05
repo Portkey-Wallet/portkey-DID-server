07c50e1fdf287d5c037b0da55c88bdc9645fe5cd
zecong.nian <duels@portkey.finance>
2023-06-05 11:54:15 +0800

    feat:add ut

--- test/CAServer.Application.Tests/Phone/PhoneInfoServiceTestsMock.cs
+++ test/CAServer.Application.Tests/Phone/PhoneInfoServiceTestsMock.cs
@@ -1,6 +1,8 @@
 using CAServer.Options;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
+using CAServer.IpInfo;
+using Moq;
 
 namespace CAServer.Phone;
 
@@ -8,15 +10,21 @@ public partial class PhoneInfoServiceTests
 {
     private IOptions<PhoneInfoOptions> GetPhoneInfoOptions()
     {
-        var phoneInfoItem = new PhoneInfoItem
-        {
-                Country = "12345678901234567890123456789012",
-                Code = "sssss",
-                Iso = "123"
-        };
         var phoneInfoOptions = new PhoneInfoOptions();
         var phoneInfo = new List<PhoneInfoItem>();
-        phoneInfo.Add(phoneInfoItem);
+        phoneInfo.Add(new PhoneInfoItem
+        {
+            Country = "Singapore",
+            Code = "65",
+            Iso = "SG"
+        });
+        phoneInfo.Add(new PhoneInfoItem()
+        {
+            
+            Country = "United States",
+            Code = "1",
+            Iso = "US"
+        });
         phoneInfoOptions.PhoneInfo = phoneInfo;
         phoneInfoOptions.Default = new PhoneInfoItem
         {
@@ -26,6 +34,36 @@ public partial class PhoneInfoServiceTests
         };
         return new OptionsWrapper<PhoneInfoOptions>(
             phoneInfoOptions);
+    }
+    
+    private IIpInfoClient GetIpInfoClient()
+    {
+
+        IpInfoDto nowhere = new IpInfoDto
+        {
+            CountryName = "MockCountry",
+            CountryCode = "NOT_FOUND",
+            Location = new LocationInfo
+            {
+                CallingCode = "404"
+            }
+        };
+        
+        IpInfoDto us = new IpInfoDto
+        {
+            CountryName = "United States",
+            CountryCode = "US",
+            Location = new LocationInfo
+            {
+                CallingCode = "1"
+            }
+        };
+
+        var ipInfoClient = new Mock<IIpInfoClient>();
+        ipInfoClient.Setup(m => m.GetIpInfoAsync(It.Is<string>(ip => ip == "0.0.0.0"))).ReturnsAsync(nowhere);
+        ipInfoClient.Setup(m => m.GetIpInfoAsync(It.Is<string>(ip => ip == "20.230.34.112"))).ReturnsAsync(us);
+
+        return ipInfoClient.Object;
     }
 }
 
