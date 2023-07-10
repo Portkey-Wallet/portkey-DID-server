50e08b350fe8c5e7d61d66e132df0f9bfdcb4854
wangyue <sean@portkey.finance>
2023-07-10 18:16:05 +0800

    feat:fix bug

--- src/CAServer.HttpApi/Controllers/CAVerifierController.cs
+++ src/CAServer.HttpApi/Controllers/CAVerifierController.cs
@@ -53,7 +53,7 @@ public class CAVerifierController : CAServerController
     {
         var type = verifierServerInput.OperationType;
         ValidateOperationType(type);
-        if (string.IsNullOrWhiteSpace(version) && version == CurrentVersion)
+        if (!string.IsNullOrWhiteSpace(version) && version == CurrentVersion)
         {
             type = type switch
             {
