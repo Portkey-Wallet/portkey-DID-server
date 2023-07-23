b2d422d1802ac9d01f762e9eff5ff9e1c683a33d
felix-zhaolei <felix@portkey.finance>
2023-07-23 15:36:48 +0800

    modify: delete useless.

--- src/CAServer.Application.Contracts/AppleMigrate/IAppleMigrateAppService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using CAServer.AppleMigrate.Dtos;
-
-namespace CAServer.AppleMigrate;
-
-public interface IAppleMigrateAppService
-{
-    Task<AppleMigrateResponseDto> MigrateAsync(AppleMigrateRequestDto input);
-}
