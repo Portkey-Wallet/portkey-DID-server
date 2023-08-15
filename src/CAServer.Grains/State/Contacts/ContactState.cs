8353cc0d394eac4533e10087b5e64166b89f4dde
felix-zhaolei <felix@portkey.finance>
2023-08-15 14:18:43 +0800

    feat: add contact field.

--- src/CAServer.Grains/State/Contacts/ContactState.cs
+++ src/CAServer.Grains/State/Contacts/ContactState.cs
@@ -1,4 +1,3 @@
-using CAServer.Etos;
 using CAServer.Grains.Grain.Contacts;
 
 namespace CAServer.Grains.State.Contacts;
@@ -8,8 +7,11 @@ public class ContactState
     public Guid Id { get; set; }
     public string Index { get; set; }
     public string Name { get; set; }
+    public string Avatar { get; set; }
     public List<ContactAddress> Addresses { get; set; } = new();
     public Guid UserId { get; set; }
+    public Guid AddedUserId {get;set;}
     public bool IsDeleted { get; set; } = true;
+    public DateTime CreateTime { get; set; }
     public DateTime ModificationTime { get; set; }
 }
