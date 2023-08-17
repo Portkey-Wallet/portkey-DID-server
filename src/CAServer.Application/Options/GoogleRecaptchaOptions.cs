b079bb4c19915222b365ec4c5165427310f43ef6
wangyue <sean@portkey.finance>
2023-08-17 20:43:15 +0800

    feat:fix CR

--- src/CAServer.Application/Options/GoogleRecaptchaOptions.cs
+++ src/CAServer.Application/Options/GoogleRecaptchaOptions.cs
@@ -4,9 +4,7 @@ namespace CAServer.Options;
 
 public class GoogleRecaptchaOptions
 {
-    public Dictionary<string,string> SecretMap { get; set; }
+    public Dictionary<string, string> SecretMap { get; set; }
 
     public string VerifyUrl { get; set; }
-
-
 }
