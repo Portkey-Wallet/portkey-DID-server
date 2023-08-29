d12fdf296c5364324d94c400a5951a3e8d6699d0
lance <lance@portkey.finance>
2023-08-29 17:15:28 +0800

    feature: add contact profile and list ut

--- test/CAServer.Application.Tests/Contact/ContactListTest.cs
+++ test/CAServer.Application.Tests/Contact/ContactListTest.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AElf.Indexing.Elasticsearch;
 using CAServer.Contacts;
-using CAServer.Grains.Grain.Contacts;
+using CAServer.Entities.Es;
 using CAServer.Security;
-using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using Volo.Abp.Users;
 using Xunit;
@@ -14,29 +15,64 @@ namespace CAServer.Contact;
 [Collection(CAServerTestConsts.CollectionDefinitionName)]
 public class ContactListTest : CAServerApplicationTestBase
 {
-    private const string DefaultFilter = "modificationTime: [2023-08-28T06:38:53.594Z TO 2023-08-29T03:08:17.947Z]";
+    private const string DefaultFilter = "modificationTime: [2023-07-28T06:38:53.594Z TO 2023-08-29T03:08:17.947Z]";
     
     private readonly IContactAppService _contactAppService;
-
+    private readonly INESTRepository<ContactIndex, Guid> _contactRepository;
+    private ICurrentUser _currentUser;
+    
     public ContactListTest()
     {
         _contactAppService = GetRequiredService<IContactAppService>();
+        _contactRepository = GetRequiredService<INESTRepository<ContactIndex, Guid>>();
+        _currentUser = new CurrentUser(new FakeCurrentPrincipalAccessor());
     }
-    
-    protected override void AfterAddApplication(IServiceCollection services)
-    {
-        services.AddSingleton(ContactListMock.BuildMockIContactAppService());
-    }
-    
+
     [Fact]
     public async Task GetList_Test()
     {
+        await MockContactListData();
+        
         var pagedResultDto = await _contactAppService.GetListAsync(new ContactGetListDto
         {
             Filter = DefaultFilter
         });
         
-        pagedResultDto.TotalCount.ShouldBe(2);
+        pagedResultDto.TotalCount.ShouldBe(1);
         pagedResultDto.Items[0].Name.ShouldBe("Test1");
+    }
+    
+    
+    [Fact]
+    public async Task GetList_Filter_NullOrEmpty_Test()
+    {
+        await MockContactListData();
+        
+        var pagedResultDto = await _contactAppService.GetListAsync(new ContactGetListDto());
+        
+        pagedResultDto.TotalCount.ShouldBe(2);
+    }
+
+    private async Task MockContactListData()
+    {
+        var list = new List<ContactIndex>
+        {
+            new()
+            {
+                Id = new Guid("3fe8e56b-e700-123e-8cb4-d014b485c1a9"),
+                UserId = _currentUser.GetId(),
+                Name = "Test1",
+                ModificationTime = new DateTime(2023, 8, 25, 0, 0, 0, DateTimeKind.Utc)
+            },
+            new()
+            {
+                Id = new Guid("3fe8e56b-e700-123e-8cb4-d014b485c1a8"),
+                UserId = _currentUser.GetId(),
+                Name = "Test2",
+                ModificationTime = new DateTime(2023, 6, 25, 0, 0, 0, DateTimeKind.Utc)
+            }
+        };
+        
+        await _contactRepository.BulkAddOrUpdateAsync(list);
     }
 }
