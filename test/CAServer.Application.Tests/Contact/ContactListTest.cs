64147e1833dd295817a204a82efebfc4d1765d95
lance <lance@portkey.finance>
2023-08-29 14:04:12 +0800

    feature: add contact profile and list ut

---
+++ test/CAServer.Application.Tests/Contact/ContactListTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using CAServer.Contacts;
+using CAServer.Grains.Grain.Contacts;
+using CAServer.Security;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Volo.Abp.Users;
+using Xunit;
+
+namespace CAServer.Contact;
+
+
+[Collection(CAServerTestConsts.CollectionDefinitionName)]
+public class ContactListTest : CAServerApplicationTestBase
+{
+    private const string DefaultFilter = "modificationTime: [2023-08-28T06:38:53.594Z TO 2023-08-29T03:08:17.947Z]";
+    
+    private readonly IContactAppService _contactAppService;
+
+    public ContactListTest()
+    {
+        _contactAppService = GetRequiredService<IContactAppService>();
+    }
+    
+    protected override void AfterAddApplication(IServiceCollection services)
+    {
+        base.AfterAddApplication(services);
+        services.AddSingleton(ContactListMock.BuildMockIContactAppService());
+    }
+    
+    [Fact]
+    public async Task GetList_Test()
+    {
+        var pagedResultDto = await _contactAppService.GetListAsync(new ContactGetListDto
+        {
+            Filter = DefaultFilter
+        });
+        
+        pagedResultDto.TotalCount.ShouldBe(2);
+        pagedResultDto.Items[0].Name.ShouldBe("Test1");
+    }
+}
