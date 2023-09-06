2f465d79a40b08b6533c1c4f6838c168e3cd22e2
wangyue <sean@portkey.finance>
2023-09-06 15:56:38 +0800

    feat:Token ImageUrl

--- src/CAServer.Application/UserAssets/Provider/ITokenImageProvider.cs
+++ src/CAServer.Application/UserAssets/Provider/ITokenImageProvider.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using CAServer.Options;
+using Microsoft.Extensions.Options;
 using Volo.Abp.DependencyInjection;
 
 namespace CAServer.UserAssets.Provider;
@@ -9,7 +12,7 @@ public interface ITokenImageProvider
 {
     public Task<string> GetTokenImageAsync(string tokenSymbol, int width = 0, int height = 0);
 
-    public Task<List<Dictionary<string, string>>> GetTokenImagesAsync(List<string> tokenSymbols, int width = 0,
+    public Task<Dictionary<string, string>> GetTokenImagesAsync(List<string> tokenSymbols, int width = 0,
         int height = 0);
 }
 
@@ -17,6 +20,14 @@ public class TokenImageProvider : ITokenImageProvider, ISingletonDependency
 {
     public const int DefaultWidth = 144;
     public const int DefaultHeight = 144;
+    private readonly TokenInfoOptions _tokenInfoOptions;
+    private readonly IImageProcessProvider _imageProcessProvider;
+
+    public TokenImageProvider(IOptions<TokenInfoOptions> tokenInfoOptions, IImageProcessProvider imageProcessProvider)
+    {
+        _imageProcessProvider = imageProcessProvider;
+        _tokenInfoOptions = tokenInfoOptions.Value;
+    }
 
     public async Task<string> GetTokenImageAsync(string tokenSymbol, int width, int height)
     {
@@ -29,11 +40,18 @@ public class TokenImageProvider : ITokenImageProvider, ISingletonDependency
         {
             height = DefaultHeight;
         }
-        
-        return "";
+        if (_tokenInfoOptions.TokenInfos.IsNullOrEmpty())
+        {
+            return string.Empty;
+        }
+
+        var tokenImageDic = _tokenInfoOptions.TokenInfos.ToDictionary(k => k.Key, v => v.Value.ImageUrl);
+        var tokenImageUrl = tokenImageDic.TryGetValue(tokenSymbol, out var imageUrl) ? imageUrl : "";
+        var result = await _imageProcessProvider.GetResizeImageAsync(tokenImageUrl, width, height, ImageResizeType.PortKey);
+        return result;
     }
 
-    public async Task<List<Dictionary<string, string>>> GetTokenImagesAsync(List<string> tokenSymbols, int width = 0, int height = 0)
+    public async Task<Dictionary<string, string>> GetTokenImagesAsync(List<string> tokenSymbols, int width, int height)
     {
         if (0 == width)
         {
@@ -44,6 +62,23 @@ public class TokenImageProvider : ITokenImageProvider, ISingletonDependency
         {
             height = DefaultHeight;
         }
-        
+        if (_tokenInfoOptions.TokenInfos.IsNullOrEmpty())
+        {
+            return new Dictionary<string, string>();
+        }
+
+        var tokenImageDic = _tokenInfoOptions.TokenInfos.ToDictionary(k => k.Key, v => v.Value.ImageUrl);
+        var tokenImageDicResult = new Dictionary<string, string>();
+        foreach (var  tokenSymbol in tokenSymbols)
+        {
+            var tokenImageUrl = tokenImageDic.TryGetValue(tokenSymbol, out var imageUrl) ? imageUrl : "";
+            if (string.IsNullOrEmpty(tokenImageUrl))
+            {
+                continue;
+            }
+            var result = await _imageProcessProvider.GetResizeImageAsync(tokenImageUrl, width, height, ImageResizeType.PortKey);
+            tokenImageDicResult.Add(tokenSymbol,result);
+        }
+        return tokenImageDicResult;
     }
 }
--- src/CAServer.Application/UserAssets/Provider/ImageResizeType.cs
+++ src/CAServer.Application/UserAssets/Provider/ImageResizeType.cs
@@ -3,5 +3,6 @@ namespace CAServer.UserAssets.Provider;
 public enum ImageResizeType
 {
     PortKey,
-    Im
+    Im,
+    Forest
 }
