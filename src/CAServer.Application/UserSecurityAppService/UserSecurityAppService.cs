a171a826158c0e18f0de68b1739dbfdb3da62472
so00up <thomson@portkey.finance>
2023-09-07 14:23:58 +0800

    add graph ql with indexer.

--- src/CAServer.Application/UserSecurityAppService/UserSecurityAppService.cs
+++ src/CAServer.Application/UserSecurityAppService/UserSecurityAppService.cs
@@ -68,16 +68,7 @@ public class UserSecurityAppService : CAServerAppService, IUserSecurityAppServic
             }
 
             // If the transferLimit is updated, the token transferLimit will be overwritten
-            // var res = await _userSecurityProvider.GetTransferLimitListByCaHash(input.CaHash);
-            var res = new IndexerTransferLimitList
-            {
-                CaHolderTransferLimit = new CaHolderTransferLimit
-                {
-                    TotalRecordCount = 0,
-                    Data = new List<TransferLimitDto>()
-                }
-            };
-
+            var res = await _userSecurityProvider.GetTransferLimitListByCaHash(input.CaHash);
             _logger.LogDebug("CaHash: {caHash} have {COUNT} transfer limit change history.", input.CaHash,
                 res.CaHolderTransferLimit.TotalRecordCount);
 
