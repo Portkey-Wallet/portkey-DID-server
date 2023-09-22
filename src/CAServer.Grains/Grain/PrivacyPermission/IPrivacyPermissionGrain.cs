c8cc214605e2f1e058c4d7effe445bf959b4eaf1
eason <page@portkey.finance>
2023-09-22 10:04:48 +0800

    fix format

--- src/CAServer.Grains/Grain/PrivacyPermission/IPrivacyPermissionGrain.cs
+++ src/CAServer.Grains/Grain/PrivacyPermission/IPrivacyPermissionGrain.cs
@@ -6,7 +6,7 @@ namespace CAServer.Grains.Grain.PrivacyPermission;
 
 public interface IPrivacyPermissionGrain : IGrainWithGuidKey
 {
-    Task<int> DeletePermissionAsync(string identifier, PrivacyType type)
+    Task<int> DeletePermissionAsync(string identifier, PrivacyType type);
     Task<List<PermissionSetting>> GetPermissionAsync(List<PermissionSetting> checkList, PrivacyType privacyType);
     Task SetPermissionAsync(PermissionSetting setting);
     Task<PrivacyPermissionDto> GetPrivacyPermissionAsync();
