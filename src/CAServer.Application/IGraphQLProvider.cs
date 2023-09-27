ff413cddfd6dd5f6bdad42ae1140ea6c95d95520
eason <page@portkey.finance>
2023-09-27 14:32:07 +0800

    move dto file

--- src/CAServer.Application/IGraphQLProvider.cs
+++ src/CAServer.Application/IGraphQLProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CAServer.CAActivity.Provider;
+using CAServer.ContractEventHandler.Core.Application;
 using CAServer.Grains.Grain.ApplicationHandler;
 using CAServer.Options;
 using GraphQL;
