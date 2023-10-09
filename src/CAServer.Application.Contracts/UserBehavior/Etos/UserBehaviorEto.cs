74a12aafd3df6def5ef2a1ee1646a8adcca3f901
eason <page@portkey.finance>
2023-10-09 15:27:57 +0800

    add UserBehavior handler

--- src/CAServer.Application.Contracts/UserBehavior/Etos/UserBehaviorEto.cs
+++ src/CAServer.Application.Contracts/UserBehavior/Etos/UserBehaviorEto.cs
@@ -7,7 +7,13 @@ public class UserBehaviorEto
 {
     public string DappName { get; set; }
     public string CaAddress { get; set; }
-    public string Action { get; set; }
-    public string Referrer { get; set; }
+    public string CaHash { get; set; }
+    public string UserId { get; set; }
+    public UserBehaviorAction Action { get; set; }
+    public string Referer { get; set; }
     public string UserAgent { get; set; }
+    public string Origin { get; set; }
+    public bool Result { get; set; }
+    public string ChainId { get; set; }
+    public string SessionId { get; set; }
 }
