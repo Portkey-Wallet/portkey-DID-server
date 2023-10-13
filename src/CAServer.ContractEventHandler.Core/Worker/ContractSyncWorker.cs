93918af410d32ab5bbfc63039e5261e437f2eaab
eason <page@portkey.finance>
2023-10-13 16:09:29 +0800

    add log

--- src/CAServer.ContractEventHandler.Core/Worker/ContractSyncWorker.cs
+++ src/CAServer.ContractEventHandler.Core/Worker/ContractSyncWorker.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using CAServer.ContractEventHandler.Core.Application;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Volo.Abp.BackgroundWorkers;
 using Volo.Abp.Threading;
@@ -11,19 +12,23 @@ public class ContractSyncWorker : AsyncPeriodicBackgroundWorkerBase
 {
     private readonly IContractAppService _contractAppService;
     private readonly ContractSyncOptions _contractSyncOptions;
+    private readonly ILogger<ContractSyncWorker> _logger;
 
     public ContractSyncWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory,
+        ILogger<ContractSyncWorker> logger,
         IContractAppService contractAppService, IOptions<ContractSyncOptions> workerOptions) : base(timer,
         serviceScopeFactory)
     {
         _contractSyncOptions = workerOptions.Value;
         _contractAppService = contractAppService;
-
+        _logger = logger;
         Timer.Period = 1000 * _contractSyncOptions.Sync;
     }
 
     protected override async Task DoWorkAsync(PeriodicBackgroundWorkerContext workerContext)
     {
+        _logger.LogInformation("QueryAndSyncAsync start==============");
         await _contractAppService.QueryAndSyncAsync();
+        _logger.LogInformation("QueryAndSyncAsync end==============");
     }
 }
