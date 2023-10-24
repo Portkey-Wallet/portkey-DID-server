0e6dd73e5366de66be7ad153fd13c8e2277a26d8
zecong.nian <duels@portkey.finance>
2023-10-24 13:43:09 +0800

    fix: merge issue

--- src/CAServer.Application.Contracts/ThirdPart/IThirdPartOrderAppService.cs
+++ src/CAServer.Application.Contracts/ThirdPart/IThirdPartOrderAppService.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using CAServer.Commons;
 using CAServer.Commons.Dtos;
 using CAServer.ThirdPart.Dtos;
+using Google.Protobuf.WellKnownTypes;
 
 namespace CAServer.ThirdPart;
 
