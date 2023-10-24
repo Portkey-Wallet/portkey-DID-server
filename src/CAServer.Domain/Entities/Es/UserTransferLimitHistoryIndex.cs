bb430d0cd589878cd44ce3a95e55fad8f7fa5469
so00up <thomson@portkey.finance>
2023-10-24 10:17:49 +0800

    fix pr.

--- src/CAServer.Domain/Entities/Es/UserTransferLimitHistoryIndex.cs
+++ src/CAServer.Domain/Entities/Es/UserTransferLimitHistoryIndex.cs
@@ -6,7 +6,7 @@ namespace CAServer.Entities.Es;
 
 public class UserTransferLimitHistoryIndex : CAServerEsEntity<Guid>, IIndexBuild
 {
-    [Keyword] public override Guid Id { get; set; }
+    public override Guid Id { get; set; }
     [Keyword] public string CaHash { get; set; }
     [Keyword] public string Symbol { get; set; }
     [Keyword] public string ChainId { get; set; }
