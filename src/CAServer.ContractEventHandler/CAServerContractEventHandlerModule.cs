84af7b86c3d988003333dd8dfa1cebafee3ce6b9
eason <page@portkey.finance>
2023-10-26 20:57:55 +0800

    add application dependency

--- src/CAServer.ContractEventHandler/CAServerContractEventHandlerModule.cs
+++ src/CAServer.ContractEventHandler/CAServerContractEventHandlerModule.cs
@@ -3,8 +3,11 @@ using CAServer.ContractEventHandler.Core;
 using CAServer.ContractEventHandler.Core.Application;
 using CAServer.ContractEventHandler.Core.Worker;
 using CAServer.Grains;
+using CAServer.MongoDB;
 using CAServer.Options;
 using CAServer.Signature;
+using Medallion.Threading;
+using Medallion.Threading.Redis;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -39,7 +42,8 @@ namespace CAServer.ContractEventHandler;
     typeof(AbpAspNetCoreSerilogModule),
     typeof(AbpEventBusRabbitMqModule),
     typeof(CAServerSignatureModule),
-    typeof(AbpCachingStackExchangeRedisModule)
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(CAServerMongoDbModule)
 )]
 public class CAServerContractEventHandlerModule : AbpModule
 {
@@ -62,13 +66,26 @@ public class CAServerContractEventHandlerModule : AbpModule
         context.Services.AddHttpClient();
         ConfigureCache(configuration);
         ConfigureDataProtection(context, configuration, hostingEnvironment);
+        ConfigureDistributedLocking(context, configuration);
     }
 
     private void ConfigureCache(IConfiguration configuration)
     {
         Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "CAServer:"; });
     }
-
+    
+    private void ConfigureDistributedLocking(
+        ServiceConfigurationContext context,
+        IConfiguration configuration)
+    {
+        context.Services.AddSingleton<IDistributedLockProvider>(sp =>
+        {
+            var connection = ConnectionMultiplexer
+                .Connect(configuration["Redis:Configuration"]);
+            return new RedisDistributedSynchronizationProvider(connection.GetDatabase());
+        });
+    }
+    
     private void ConfigureDataProtection(
         ServiceConfigurationContext context,
         IConfiguration configuration,
