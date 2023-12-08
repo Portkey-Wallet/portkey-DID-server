74ea1519cc32db4d1b22bff399bf6857a744a282
felix-zhaolei <felix@portkey.finance>
2023-12-08 16:28:41 +0800

    modify: message push.

--- src/CAServer.Application/DataReporting/DataReportingAppService.cs
+++ src/CAServer.Application/DataReporting/DataReportingAppService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using CAServer.Common;
 using CAServer.Commons;
@@ -28,25 +27,18 @@ public class DataReportingAppService : CAServerAppService, IDataReportingAppServ
 
     public async Task ReportDeviceInfoAsync(UserDeviceReportingDto input)
     {
-        Logger.LogDebug("reportDeviceInfo, userId: {userId}, deviceId: {deviceId}, data: {data}", input.UserId,
-            input.DeviceId, JsonConvert.SerializeObject(input));
         input.NetworkType = _options.Network;
-
         await _requestProvider.PostAsync(MessagePushConstant.ReportDeviceInfoUri, input);
     }
 
     public async Task ReportAppStatusAsync(AppStatusReportingDto input)
     {
-        Logger.LogDebug("reportAppStatus, userId: {userId}, deviceId: {deviceId}, status: {status}", input.UserId,
-            input.DeviceId, input.Status.ToString());
         input.NetworkType = _options.Network;
-
         await _requestProvider.PostAsync(MessagePushConstant.ReportAppStatusUri, input);
     }
 
     public async Task ExitWalletAsync(string deviceId, Guid userId)
     {
-        // delete
         Logger.LogDebug("exitWallet, userId: {userId}, deviceId: {deviceId}", userId, deviceId);
         await _requestProvider.PostAsync(MessagePushConstant.ExitWalletUri, new { userId, deviceId, _options.Network });
     }
