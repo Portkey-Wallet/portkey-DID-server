b454e66ba6d7cd81054abb8e024aa09ff4f7cdc2
daiyabin <jashkenas712@portkey.finance>
2023-12-11 15:00:42 +0800

    feature:fix bug

--- src/CAServer.Application/RedPackage/RedPackageAppService.cs
+++ src/CAServer.Application/RedPackage/RedPackageAppService.cs
@@ -290,7 +290,7 @@ public class RedPackageAppService : CAServerAppService, IRedPackageAppService
         
         await BuildAvatarAndNameAsync(detail);
         
-        if (detail.Status == RedPackageStatus.Expired || DateTimeOffset.Now.ToUnixTimeMilliseconds() > detail.ExpireTime)
+        if (detail.Status == RedPackageStatus.Expired)
         {
             detail.IsRedPackageExpired = true;
             detail.Status = RedPackageStatus.Expired;
