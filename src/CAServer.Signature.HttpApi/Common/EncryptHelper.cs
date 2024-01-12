adf9d5aa5086381627f114b7be092e8c523d8354
zecong.nian <duels@portkey.finance>
2024-01-12 22:13:54 +0800

    feat: encrypt helper

--- src/CAServer.Signature.HttpApi/Common/EncryptHelper.cs
+++ src/CAServer.Signature.HttpApi/Common/EncryptHelper.cs
@@ -8,6 +8,10 @@ namespace SignatureServer.Common;
 
 public static class EncryptHelper
 {
+    private static readonly int KeySize = 256;
+    private static readonly int BlockSize = 128;
+    private static readonly int Iterations = 1000;
+
   
     public static byte[] AesGcmEncrypt(byte[] data, byte[] key, byte[] nonce, out byte[] tag)
     {
@@ -15,7 +19,6 @@ public static class EncryptHelper
         tag = new byte[16];
         var encryptedData = new byte[data.Length];
         aesGcm.Encrypt(nonce, data, encryptedData, tag);
-
         return encryptedData;
     }
 
@@ -24,52 +27,40 @@ public static class EncryptHelper
         using var aesGcm = new AesGcm(key);
         var decryptedData = new byte[encryptedData.Length];
         aesGcm.Decrypt(nonce, encryptedData, tag, decryptedData);
-
         return decryptedData;
     }
-    
-    public static byte[] AesCbcEncrypt(byte[] sourceData, byte[] password, byte[] salt)
+ 
+    public static byte[] AesCbcEncrypt(byte[] sourceData, byte[] password, byte[]? salt = null)
     {
-        if (sourceData.IsNullOrEmpty())
-        {
-            return sourceData;
-        }
-
-        using var key = new Rfc2898DeriveBytes(password.ToHex(), salt);
-        using var aesAlg = new RijndaelManaged();
-        aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+        using var aesAlg = Aes.Create();
+        aesAlg.KeySize = KeySize;
+        aesAlg.BlockSize = BlockSize;
         aesAlg.Mode = CipherMode.CBC;
         aesAlg.Padding = PaddingMode.PKCS7;
-
+        aesAlg.Key = new Rfc2898DeriveBytes(password, salt ?? new byte[8], Iterations).GetBytes(KeySize / 8);
+        aesAlg.GenerateIV();
         var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
         using var msEncrypt = new MemoryStream();
         msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
-
         using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
         {
             csEncrypt.Write(sourceData, 0, sourceData.Length);
         }
-
         return msEncrypt.ToArray();
     }
 
-    public static byte[] AesCbcDecrypt(byte[] encryptData, byte[] password, byte[] salt)
+    public static byte[] AesCbcDecrypt(byte[] encryptData, byte[] password, byte[]? salt = null)
     {
-        if (encryptData.IsNullOrEmpty())
-        {
-            return encryptData;
-        }
-
-        using var key = new Rfc2898DeriveBytes(password.ToHex(), salt);
-        using var aesAlg = new RijndaelManaged();
+        using var aesAlg = Aes.Create();
+        aesAlg.KeySize = KeySize;
+        aesAlg.BlockSize = BlockSize;
         aesAlg.Mode = CipherMode.CBC;
         aesAlg.Padding = PaddingMode.PKCS7;
-
+        aesAlg.Key = new Rfc2898DeriveBytes(password, salt ?? new byte[8], Iterations).GetBytes(KeySize / 8);
         var iv = new byte[aesAlg.BlockSize / 8];
         Array.Copy(encryptData, 0, iv, 0, iv.Length);
         aesAlg.IV = iv;
-
-        var decryptor = aesAlg.CreateDecryptor(key.GetBytes(aesAlg.KeySize / 8), iv);
+        var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, iv);
         using var msDecrypt = new MemoryStream();
         using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
         {
@@ -77,5 +68,4 @@ public static class EncryptHelper
         }
         return msDecrypt.ToArray();
     }
-    
 }
---
+++ src/CAServer.Signature.HttpApi/Options/AuthorityOptions.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace SignatureServer.Options;
+
+public class AuthorityOptions
+{
+    
+    // appid => appSecret
+    private Dictionary<string, string> Dapp = new();
+    
+}
