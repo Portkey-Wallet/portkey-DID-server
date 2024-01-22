486b6cab106ef2f90d1e30c1c8330ff7d7d98c67
wangyue <sean@portkey.finance>
2024-01-22 21:49:17 +0800

    feat:fix IsUnlimitedDelegate

--- src/CAServer.Grains/CAServerGrainsAutoMapperProfile.cs
+++ src/CAServer.Grains/CAServerGrainsAutoMapperProfile.cs
@@ -68,7 +68,7 @@ public class CAServerGrainsAutoMapperProfile : Profile
                     e.ProjectDelegateInfo.Delegations
                 },
                 Timestamp = DateTimeOffset.FromUnixTimeSeconds(e.ProjectDelegateInfo.TimeStamp).ToTimestamp(),
-                IsUnlimitedDelegate = true,
+                IsUnlimitedDelegate = e.ProjectDelegateInfo.IsUnlimitedDelegate,
                 Signature = e.ProjectDelegateInfo.Signature
             }))
             .ForMember(d => d.GuardianApproved, opt => opt.MapFrom(e => new GuardianInfo
