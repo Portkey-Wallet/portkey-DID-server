135d1525db9c0fe865b2326f111a2d4ded17acc6
felix-zhaolei <felix@portkey.finance>
2024-01-30 11:03:45 +0800

    modify: twitter.

--- src/CAServer.Application.Contracts/TwitterAuth/Dtos/TwitterAuthDto.cs
+++ src/CAServer.Application.Contracts/TwitterAuth/Dtos/TwitterAuthDto.cs
@@ -1,8 +1,12 @@
+using Newtonsoft.Json;
+
 namespace CAServer.TwitterAuth.Dtos;
 
 public class TwitterAuthDto
 {
     public string State { get; set; }
     public string Code { get; set; }
+    
+    [JsonProperty("access_token")]
     public string AccessToken { get; set; }
 }
