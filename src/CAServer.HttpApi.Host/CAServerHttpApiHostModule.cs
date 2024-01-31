2782cd19564b1d77912d39db2d5f8a1ce5466555
wangyue <sean@portkey.finance>
2024-01-31 11:44:52 +0800

    feat:add facebook auth

--- src/CAServer.HttpApi.Host/CAServerHttpApiHostModule.cs
+++ src/CAServer.HttpApi.Host/CAServerHttpApiHostModule.cs
@@ -339,11 +339,11 @@ public class CAServerHttpApiHostModule : AbpModule
         app.UseAuthorization();
         if (!env.IsDevelopment())
         {
-            //app.UseMiddleware<RealIpMiddleware>();
+            app.UseMiddleware<RealIpMiddleware>();
         }
 
             
-        //if (env.IsDevelopment())
+        if (env.IsDevelopment())
         {
             app.UseSwagger();
             app.UseAbpSwaggerUI(options =>
