5b07542a5358dd7ff7750dca130e5ff5715df343
wangyue <sean@portkey.finance>
2024-01-31 13:39:41 +0800

    feat:facebook add auth

--- src/CAServer.Application/Facebook/FacebookAuthAppService.cs
+++ src/CAServer.Application/Facebook/FacebookAuthAppService.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using CAServer.Facebook.Dtos;
-using CAServer.Options;
 using CAServer.Verifier;
 using CAServer.Verifier.Dtos;
 using Microsoft.Extensions.Logging;
@@ -21,6 +20,7 @@ public class FacebookAuthAppService : CAServerAppService, IFacebookAuthAppServic
     private readonly FacebookOptions _facebookOptions;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<FacebookAuthAppService> _logger;
+    // private readonly ISecretProvider _secretProvider;
 
     public FacebookAuthAppService(IOptionsSnapshot<FacebookOptions> facebookOptions,
         IHttpClientFactory httpClientFactory, ILogger<FacebookAuthAppService> logger)
@@ -31,12 +31,20 @@ public class FacebookAuthAppService : CAServerAppService, IFacebookAuthAppServic
     }
 
 
-    public async Task<FacebookAuthResponse> ReceiveAsync(FacebookAuthDto authDto)
+    public async Task<FacebookAuthResponse> ReceiveAsync(string code, ApplicationType applicationType)
     {
+        //var secret = await _secretProvider.GetSecretWithCacheAsync(_facebookOptions.AppId);
+        var redirectUrl = applicationType switch
+        {
+            ApplicationType.Receive => _facebookOptions.RedirectUrl,
+            ApplicationType.UnifyReceive => _facebookOptions.UnifyRedirectUrl,
+            _ => _facebookOptions.RedirectUrl
+        };
+
         var url = "https://graph.facebook.com/v19.0/oauth/access_token?client_id=" + _facebookOptions.AppId +
-                  "&redirect_uri=" + _facebookOptions.RedirectUrl +
+                  "&redirect_uri=" + redirectUrl +
                   "/&client_secret=" + _facebookOptions.AppSecret +
-                  "&code=" + authDto.Code;
+                  "&code=" + code;
 
         var result = await HttpRequestAsync(url);
         var facebookOauthInfo = JsonConvert.DeserializeObject<FacebookOauthResponse>(result);
--- src/CAServer.HttpApi/Controllers/FacebookAuthController.cs
+++ src/CAServer.HttpApi/Controllers/FacebookAuthController.cs
@@ -1,9 +1,9 @@
 using System.Threading.Tasks;
-using CAServer.AppleAuth.Dtos;
-using CAServer.Commons;
 using CAServer.Facebook;
 using CAServer.Facebook.Dtos;
+using CAServer.Verifier;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Volo.Abp;
 
@@ -17,30 +17,33 @@ public class FacebookAuthController : CAServerController
 {
     private readonly IFacebookAuthAppService _facebookAuthAppService;
     private readonly FacebookOptions _facebookOptions;
+    private readonly ILogger<FacebookAuthController> _logger;
 
     public FacebookAuthController(IFacebookAuthAppService facebookAuthAppService,
-        IOptionsSnapshot<FacebookOptions> facebookOptions)
+        IOptionsSnapshot<FacebookOptions> facebookOptions, ILogger<FacebookAuthController> logger)
     {
         _facebookAuthAppService = facebookAuthAppService;
+        _logger = logger;
         _facebookOptions = facebookOptions.Value;
     }
 
-    [HttpPost("receive")]
-    public async Task<IActionResult> ReceiveAsync([FromForm] FacebookAuthDto authDto)
+    [HttpGet("receive")]
+    public async Task<IActionResult> ReceiveAsync(string code)
     {
-        var result = await _facebookAuthAppService.ReceiveAsync(authDto);
+        var result = await _facebookAuthAppService.ReceiveAsync(code, ApplicationType.Receive);
         var redirectUrl = _facebookOptions.RedirectUrl + "/portkey-auth-callback?token=" + result.UserId + "." +
                           result.AccessToken + "." + result.ExpiresTime + ".&type=facebook";
+        _logger.LogInformation("RedirectUrl is {url}: ", redirectUrl);
         return Redirect(redirectUrl);
     }
 
-    [HttpPost("unifyReceive")]
-    public async Task<IActionResult> UnifyReceiveAsync([FromForm] FacebookAuthDto authDto)
+    [HttpGet("unifyReceive")]
+    public async Task<IActionResult> UnifyReceiveAsync(string code)
     {
-        var result = await _facebookAuthAppService.ReceiveAsync(authDto);
-        var redirectUrl = _facebookOptions.UnifyRedirectUrl + "/portkey-auth-callback?token=" + result.UserId + "." +
+        var result = await _facebookAuthAppService.ReceiveAsync(code, ApplicationType.UnifyReceive);
+        var redirectUrl = _facebookOptions.UnifyRedirectUrl + "/auth-callback?token=" + result.UserId + "." +
                           result.AccessToken + "." + result.ExpiresTime + ".&type=facebook";
-        ;
+        _logger.LogInformation("RedirectUrl is {url}: ", redirectUrl);
         return Redirect(redirectUrl);
     }
 }
