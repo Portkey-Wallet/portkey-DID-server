dd3a5272cdcd048f6d04fd59dfb1ab639af0069e
chaoxkang <iliad@portkey.finance>
2024-03-20 23:46:36 +0800

    feat: remove default ipfs

--- src/CAServer.Application.Contracts/Commons/IpfsImageUrlHelper.cs
+++ src/CAServer.Application.Contracts/Commons/IpfsImageUrlHelper.cs
@@ -3,13 +3,12 @@ namespace CAServer.Commons;
 public static class IpfsImageUrlHelper
 {
     private const string OriginalIpfsPrefix = "ipfs://";
-    private const string ReplacedIpfsPrefix = "https://ipfs.io/ipfs/";
     
-    public static string TryGetIpfsImageUrl(string imageUrl, string replacedIpfsPrefix = null)
+    public static string TryGetIpfsImageUrl(string imageUrl, string replacedIpfsPrefix)
     {
         if (!string.IsNullOrEmpty(imageUrl) && imageUrl.StartsWith(OriginalIpfsPrefix))
         {
-            return replacedIpfsPrefix ?? ReplacedIpfsPrefix + imageUrl.Substring(OriginalIpfsPrefix.Length);
+            return replacedIpfsPrefix + imageUrl.Substring(OriginalIpfsPrefix.Length);
         }
         return imageUrl;
     }
