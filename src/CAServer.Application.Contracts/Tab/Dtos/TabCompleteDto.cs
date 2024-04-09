c177a1547b012d467a55647d1c325ca5178a3db2
felix-zhaolei <felix@portkey.finance>
2024-04-09 14:02:33 +0800

    modify: tab communication.

--- src/CAServer.Application.Contracts/Tab/Dtos/TabCompleteDto.cs
+++ src/CAServer.Application.Contracts/Tab/Dtos/TabCompleteDto.cs
@@ -8,4 +8,5 @@ public class TabCompleteDto
     [Required] public string MethodName { get; set; }
     public string Data { get; set; }
     public bool NeedPersist { get; set; }
+    public string TargetClientId { get; set; }
 }
--- src/CAServer.HttpApi/HubsEventHandler/TabHandler.cs
+++ src/CAServer.HttpApi/HubsEventHandler/TabHandler.cs
@@ -27,22 +27,46 @@ public class TabHandler : IDistributedEventHandler<TabCompleteEto>, ITransientDe
     {
         try
         {
-            var connectId = await _hubService.GetConnectionIdAsync(eventData.ClientId);
-            if (connectId.IsNullOrEmpty())
+            await SendAsync(eventData.ClientId, eventData.MethodName, eventData.Data);
+            if (!eventData.TargetClientId.IsNullOrEmpty())
             {
-                _logger.LogWarning("connectId not exist, clientId:{clientId}", eventData.ClientId);
-                return;
+                _logger.LogInformation(
+                    "begin send to targetClientId, clientId:{clientId}, methodName:{methodName}, targetClientId:{targetClientId}",
+                    eventData.ClientId, eventData.MethodName, eventData.TargetClientId);
+                
+                await SendAsync(eventData.TargetClientId, eventData.MethodName, eventData.Data);
             }
-
-            var data = new HubResponse<string> { Body = eventData.Data, RequestId = eventData.ClientId };
-            await _hubContext.Clients.Client(connectId).SendAsync(eventData.MethodName, data);
-            _logger.LogInformation("communication success, clientId:{clientId}, methodName:{methodName}",
-                eventData.ClientId, eventData.MethodName);
         }
         catch (Exception e)
         {
             _logger.LogError(e, "communication success, clientId:{clientId}, methodName:{methodName}",
                 eventData.ClientId, eventData.MethodName);
+        }
+    }
+
+    private async Task SendAsync(string clientId, string methodName, string data)
+    {
+        var connectId = await _hubService.GetConnectionIdAsync(clientId);
+        if (connectId.IsNullOrEmpty())
+        {
+            _logger.LogWarning("connectId not exist, clientId:{clientId}", clientId);
+        }
+
+        await SendAsync(connectId, clientId, methodName, data);
+    }
+
+    private async Task SendAsync(string connectId, string clientId, string methodName, string data)
+    {
+        try
+        {
+            await _hubContext.Clients.Client(connectId).SendAsync(methodName,
+                new HubResponse<string> { Body = data, RequestId = clientId });
+            _logger.LogInformation("send success, clientId:{clientId}, methodName:{methodName}", clientId,
+                methodName);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "send error, clientId:{clientId}, methodName:{methodName}", clientId, methodName);
         }
     }
 }
