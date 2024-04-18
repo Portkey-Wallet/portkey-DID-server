c7b2b19460a5847ff5470e53feaa4005cfb8fefa
felix-zhaolei <felix@portkey.finance>
2024-04-18 16:19:02 +0800

    modify: keyword ignore case.

--- src/CAServer.Application/Tokens/UserTokenAppService.cs
+++ src/CAServer.Application/Tokens/UserTokenAppService.cs
@@ -144,7 +144,7 @@ public class UserTokenAppService : CAServerAppService, IUserTokenAppService
 
         if (!requestDto.Keyword.IsNullOrEmpty())
         {
-            tokens = tokens.Where(t => t.Symbol.Contains(requestDto.Keyword)).ToList();
+            tokens = tokens.Where(t => t.Symbol.ToUpper().Contains(requestDto.Keyword.ToUpper())).ToList();
         }
 
         if (!requestDto.ChainIds.IsNullOrEmpty())
