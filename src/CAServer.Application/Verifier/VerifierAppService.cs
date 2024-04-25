6a8c438805180cf8f7e1fe6fbcd3116c43c554ac
wangyue <sean@portkey.finance>
2024-04-25 18:38:06 +0800

    feat:remove unused code

--- src/CAServer.Application/Verifier/VerifierAppService.cs
+++ src/CAServer.Application/Verifier/VerifierAppService.cs
@@ -296,12 +296,6 @@ public class VerifierAppService : CAServerAppService, IVerifierAppService
     }
 
 
-    public async Task<bool> VerifyRevokeCodeAsync(VerifyRevokeCodeInput input)
-    {
-        return await _verifierServerClient.VerifyRevokeCodeAsync(input);
-
-    }
-
     private async Task<string> GetTwitterUserIdAsync(string accessToken)
     {
         var header = new Dictionary<string, string>
