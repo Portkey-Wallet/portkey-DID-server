616be1507bf087a973ee43ec36440e98367bcbb1
wangyue <sean@portkey.finance>
2024-04-25 14:01:45 +0800

    feat:add UT

--- test/CAServer.Application.Tests/VerifierCode/VerifierServerClientTests.cs
+++ test/CAServer.Application.Tests/VerifierCode/VerifierServerClientTests.cs
@@ -136,4 +136,18 @@ public partial class VerifierServerClientTests : CAServerApplicationTestBase
 
         await _verifierServerClient.VerifyAppleTokenAsync(input, identifierHash, salt);
     }
+    
+    [Fact]
+    public async Task VerifyRevokeToken_Test()
+    {
+        var input = new VerifyRevokeCodeInput()
+        {
+            VerifierId = DefaultVerifierId,
+            ChainId = DefaultChainId,
+            VerifyCode = 
+                "eyJraWQiOiJXNldjT0tCIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLnBvcnRrZXkuZGlkIiwiZXhwIjoxNjc5NDcyMjg1LCJpYXQiOjE2NzkzODU4ODUsInN1YiI6IjAwMDMwMy5jZDgxN2I2OTgzMDc0ZDhjOGZiNzkyNDk2ZjI3N2ViYy4wMjU3IiwiY19oYXNoIjoicFBSeFFTSWNWY19BTEExSE9vdmJ5QSIsImVtYWlsIjoicHQ2eXhtOXptbUBwcml2YXRlcmVsYXkuYXBwbGVpZC5jb20iLCJlbWFpbF92ZXJpZmllZCI6InRydWUiLCJpc19wcml2YXRlX2VtYWlsIjoidHJ1ZSIsImF1dGhfdGltZSI6MTY3OTM4NTg4NSwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.wXHXNbQVqvRxK_a6dq3WjBbJe_KaGsRVgSz_i3E01JyKW8rxGRRgDqjYNiTxB6iOqBMfvXfjtjgPl1N-de_Q4OflzG7gKK_17c-sY2uXUbOWVtAFI9WEXksYhZdV66eJDiUKJ8KE94S6NCT8UdkRqtxHtCnjuq82taYPbqcb-NO3Xcu23hfKsYQM_73yHJfnFd7jUYCoLHcxlVUeRGR7D7L3Yo9FdbocHZwei_x_jwb_7gYjTqGKg6rYt4MRT5ElSTj4xajXrRLZZzCFTVPjytvUsGvU038SEj4sIK6eoDAQy90ne2_XritzViMfKcWid6cdgh-Zz3PzfRx9LEyIPg"
+        };
+        var result = await _verifierServerClient.VerifyRevokeCodeAsync(input);
+        result.ShouldBe(false);
+    }
 }
