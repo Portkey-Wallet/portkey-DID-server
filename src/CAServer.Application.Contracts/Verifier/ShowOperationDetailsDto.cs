d69924775733a29236be47204c3fddf7ce03f615
wangyue <sean@portkey.finance>
2024-05-08 20:36:40 +0800

    feat:add show operationdetail

---
+++ src/CAServer.Application.Contracts/Verifier/ShowOperationDetailsDto.cs
@@ -0,0 +1,21 @@
+namespace CAServer.Verifier;
+
+public class ShowOperationDetailsDto
+{
+    public string IdentifierHash { get; set; }
+
+    public string GuardianType { get; set; }
+
+    public string Time { get; set; }
+
+    public string Chain { get; set; }
+
+    public string Token { get; set; }
+
+    public string Amount { get; set; }
+
+    public string IP { get; set; }
+
+    public string OperationType { get; set; }
+    
+}
