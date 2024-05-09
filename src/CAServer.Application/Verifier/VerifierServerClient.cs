e88129f2b67bc070c4c9f5f0f3fda30ea50e37bb
wangyue <sean@portkey.finance>
2024-05-09 22:07:19 +0800

    guardianapprove verify operatedetail (#262)
    guardianapprove verify operatedetail

--- src/CAServer.Application/Verifier/VerifierServerClient.cs
+++ src/CAServer.Application/Verifier/VerifierServerClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 using System.Net.Http;
 using System.Net.Mime;
 using System.Text;
@@ -9,11 +10,14 @@ using AElf;
 using CAServer.Accelerate;
 using CAServer.Common;
 using CAServer.Dtos;
+using CAServer.IpInfo;
+using CAServer.Options;
 using CAServer.Settings;
 using CAServer.Verifier.Dtos;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Volo.Abp;
 using Volo.Abp.DependencyInjection;
 
@@ -26,20 +30,23 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
     private readonly ILogger<VerifierServerClient> _logger;
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly IAccelerateManagerProvider _accelerateManagerProvider;
-    private readonly IContractProvider _contractProvider;
+    private readonly ChainOptions _chainOptions;
+    private readonly IIpInfoAppService _ipInfoAppService;
 
 
     public VerifierServerClient(IOptionsSnapshot<AdaptableVariableOptions> adaptableVariableOptions,
         IGetVerifierServerProvider getVerifierServerProvider,
         ILogger<VerifierServerClient> logger,
-        IHttpClientFactory httpClientFactory, IAccelerateManagerProvider accelerateManagerProvider, IContractProvider contractProvider)
+        IHttpClientFactory httpClientFactory, IAccelerateManagerProvider accelerateManagerProvider,
+        IOptions<ChainOptions> chainOptions, IIpInfoAppService ipInfoAppService)
     {
         _getVerifierServerProvider = getVerifierServerProvider;
         _logger = logger;
         _httpService = new HttpService(adaptableVariableOptions.Value.HttpConnectTimeOut, httpClientFactory, true);
         _httpClientFactory = httpClientFactory;
         _accelerateManagerProvider = accelerateManagerProvider;
-        _contractProvider = contractProvider;
+        _ipInfoAppService = ipInfoAppService;
+        _chainOptions = chainOptions.Value;
     }
 
     private bool _disposed;
@@ -74,15 +81,36 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
             };
         }
 
+        var operationDetails =
+            _accelerateManagerProvider.GenerateOperationDetails(dto.OperationType, dto.OperationDetails);
+        var showOperationDetails = new ShowOperationDetailsDto
+        {
+            OperationType = GetOperationDecs(dto.OperationType),
+            Token = GetDetailDesc(dto.OperationDetails, "symbol"),
+            Amount = GetDetailDesc(dto.OperationDetails, "amount"),
+            Chain = GetChainDetailDesc(dto.ChainId),
+            GuardianType = dto.Type,
+            GuardianAccount = dto.GuardianIdentifier,
+            Time = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss", CultureInfo.InvariantCulture),
+            IP = await GetIpDetailDesc()
+        };
+
+        var showOperationDetailsJson = JsonConvert.SerializeObject(showOperationDetails);
+
         var url = endPoint + "/api/app/account/sendVerificationRequest";
         var parameters = new Dictionary<string, string>
         {
             { "type", dto.Type },
             { "guardianIdentifier", dto.GuardianIdentifier },
             { "verifierSessionId", dto.VerifierSessionId.ToString() },
+            { "operationDetails", operationDetails },
+            { "showOperationDetails", showOperationDetailsJson }
         };
         return await _httpService.PostResponseAsync<ResponseResultDto<VerifierServerResponse>>(url, parameters);
     }
+
+
+
 
     public async Task<ResponseResultDto<VerificationCodeResponse>> VerifyCodeAsync(VierifierCodeRequestInput input)
     {
@@ -161,6 +189,7 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
                 Message = "No Available Service Tips."
             };
         }
+
         var url = endPoint + "/api/app/account/verifyFacebookAccessTokenAndGetUserId";
         var parameters = new Dictionary<string, string>
         {
@@ -171,7 +200,8 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
         return await _httpService.PostResponseAsync<ResponseResultDto<VerifyFacebookUserInfoDto>>(url, parameters);
     }
 
-    public async Task<ResponseResultDto<VerifyTwitterTokenDto>> VerifyTwitterTokenAsync(VerifyTokenRequestDto input, string identifierHash, string salt)
+    public async Task<ResponseResultDto<VerifyTwitterTokenDto>> VerifyTwitterTokenAsync(VerifyTokenRequestDto input,
+        string identifierHash, string salt)
     {
         var requestUri = "/api/app/account/verifyTwitterToken";
         return await GetResultAsync<VerifyTwitterTokenDto>(input, requestUri, identifierHash, salt);
@@ -217,7 +247,7 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
         }
 
         var url = endPoint + requestUri;
-        
+
         var operationDetails =
             _accelerateManagerProvider.GenerateOperationDetails(input.OperationType, input.OperationDetails);
 
@@ -259,4 +289,75 @@ public class VerifierServerClient : IDisposable, IVerifierServerClient, ISinglet
 
         return JsonConvert.DeserializeObject<ResponseResultDto<T>>(result);
     }
+
+    private string GetOperationDecs(OperationType operationType)
+    {
+        return operationType switch
+        {
+            OperationType.CreateCAHolder => "Create AA Address",
+            OperationType.SocialRecovery => "Social Recovery",
+            OperationType.AddGuardian => "Add Guardian",
+            OperationType.RemoveGuardian => "Remove Guardian",
+            OperationType.UpdateGuardian => "Update Guardian",
+            OperationType.RemoveOtherManagerInfo => "Remove device",
+            OperationType.SetLoginGuardian => "Set login account",
+            OperationType.Approve => "Approve",
+            OperationType.ModifyTransferLimit => "Set Transfer Limit",
+            OperationType.GuardianApproveTransfer => "Guardian Approve Transfer",
+            OperationType.UnSetLoginAccount => "Unset login account",
+            _ => ""
+        };
+    }
+
+    private string GetDetailDesc(string dtoOperationDetails, string keyword)
+    {
+        try
+        {
+            JsonConvert.DeserializeObject(dtoOperationDetails);
+            var jsonObj = JObject.Parse(dtoOperationDetails);
+            foreach (var child in jsonObj.Children())
+            {
+                if (child is not JProperty property)
+                {
+                    continue;
+                }
+
+                if (property.Name != keyword)
+                {
+                    continue;
+                }
+
+                var response = property.Value.ToString();
+                return response;
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogDebug("DeserializeObject Json failed : {json}, Error Message is : {message}",
+                dtoOperationDetails, e.Message);
+            return "";
+        }
+
+        return "";
+    }
+
+    private string GetChainDetailDesc(string chain)
+    {
+        var chainDetails = _chainOptions.ChainInfos.TryGetValue(chain, out var chainInfo);
+        if (chainDetails)
+        {
+            var isMainChain = chainInfo.IsMainChain;
+            return isMainChain ? "MainChain " + chainInfo.ChainId : "SideChain " + chainInfo.ChainId;
+        }
+
+        _logger.LogError("GetChainInfo Error:{chainId}", chain);
+        return "";
+    }
+
+    private async Task<string> GetIpDetailDesc()
+    {
+        var ipInfo = await _ipInfoAppService.GetIpInfoAsync();
+        return ipInfo.Country;
+    }
+    
 }
