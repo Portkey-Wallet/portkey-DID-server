55892e7bb0968f95a82cdd7dae8644f3b2a1a8f1
felix-zhaolei <felix@portkey.finance>
2024-05-17 17:02:00 +0800

    modify: sort token list.

--- src/CAServer.Application/Tokens/TokenDisplayAppService.cs
+++ src/CAServer.Application/Tokens/TokenDisplayAppService.cs
@@ -818,7 +818,8 @@ public class TokenDisplayAppService : CAServerAppService, ITokenDisplayAppServic
         var defaultSymbols = _tokenListOptions.UserToken.Select(t => t.Token.Symbol).Distinct().ToList();
         var sourceSymbols = _tokenListOptions.SourceToken.Select(t => t.Token.Symbol).Distinct().ToList();
 
-        return tokens.OrderBy(t => t.Symbol != CommonConstant.ELF)
+        return tokens.OrderBy(t => decimal.Parse(t.Balance) == 0)
+            .ThenBy(t => t.Symbol != CommonConstant.ELF)
             .ThenBy(t => !defaultSymbols.Contains(t.Symbol))
             .ThenBy(t => sourceSymbols.Contains(t.Symbol))
             .ThenBy(t => Array.IndexOf(defaultSymbols.ToArray(), t.Symbol))
