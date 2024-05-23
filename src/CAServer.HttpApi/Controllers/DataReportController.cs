3603ee7c7c09d895b07d02739a1c0d8e171f3ecc
felix-zhaolei <felix@portkey.finance>
2024-05-23 22:23:04 +0800

    feat: add exit wallet data reporting.

---
+++ src/CAServer.HttpApi/Controllers/DataReportController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using CAServer.DataReporting.Dtos;
+using CAServer.DataReporting.Etos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.Users;
+
+namespace CAServer.Controllers;
+
+[RemoteService]
+[Area("app")]
+[ControllerName("DataReport")]
+[Route("api/app/report")]
+public class DataReportController : CAServerController
+{
+    private readonly IDistributedEventBus _eventBus;
+
+    public DataReportController(IDistributedEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    [HttpGet("exitWallet"), Authorize]
+    public async Task ExitWalletAsync(ExitWalletDto exitWalletDto)
+    {
+        await _eventBus.PublishAsync(new ExitWalletEto
+        {
+            UserId = CurrentUser.GetId(),
+            DeviceId = exitWalletDto.DeviceId
+        });
+    }
+}
