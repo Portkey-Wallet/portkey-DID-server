639781c2bec95b977ae66f0de106e3f8013a5b39
felix-zhao <123852192+felix-zhaolei@users.noreply.github.com>
2024-05-27 14:05:53 +0800

    fix: deposit network confirmation. (#338)

--- src/CAServer.Application/Options/DepositOptions.cs
+++ src/CAServer.Application/Options/DepositOptions.cs
@@ -6,4 +6,11 @@ public class DepositOptions
 {
     public List<string> ExtraNotes { get; set; }
     public List<string> SwapExtraNotes { get; set; }
+    public Dictionary<string, WordReflection> Reflection { get; set; }
+}
+
+public class WordReflection
+{
+    public string Include { get; set; }
+    public string NextWord { get; set; }
 }
