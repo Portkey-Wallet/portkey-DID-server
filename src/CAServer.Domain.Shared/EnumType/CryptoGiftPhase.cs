d2acd1609a1b5dc9fa730470a71574d36db2bc9c
Edward-BXS <Magic.Wizard@portkey.finance>
2024-06-13 15:27:09 +0800

    fix

--- src/CAServer.Domain.Shared/EnumType/CryptoGiftPhase.cs
+++ src/CAServer.Domain.Shared/EnumType/CryptoGiftPhase.cs
@@ -5,11 +5,12 @@ namespace CAServer.EnumType;
  */
 public enum CryptoGiftPhase
 {
-    Available, //not claimed and not expired
-    FullyClaimed,
-    Expired,
-    NoQuota,
-    GrabbedQuota, //claimed but have enough quota
+    Available = 0, //not claimed and not expired
+    Expired, //the red package is expired
     Claimed, //the client has received the gift
     ExpiredReleased, //the quota is expired, so it's released
+    FullyClaimed, //the red package has been fully claimed
+    OnlyNewUsers, //the crypto gift is only for new users
+    GrabbedQuota, //claimed but have enough quota
+    NoQuota, //
 }
