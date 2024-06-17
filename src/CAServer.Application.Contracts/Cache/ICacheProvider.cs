1245640b6357ff659d85db6c8e73d41c011a5479
wangyue <sean@portkey.finance>
2024-06-17 14:15:15 +0800

    feat:fix redis error

--- src/CAServer.Application.Contracts/Cache/ICacheProvider.cs
+++ src/CAServer.Application.Contracts/Cache/ICacheProvider.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using StackExchange.Redis;
+using Volo.Abp.DependencyInjection;
 
 namespace CAServer.Cache;
 
-public interface ICacheProvider
+public interface ICacheProvider : ISingletonDependency
 {
     Task HSetWithExpire(string key, string member, string value, TimeSpan? expire);
     Task<bool> HashDeleteAsync(string key, string member);
