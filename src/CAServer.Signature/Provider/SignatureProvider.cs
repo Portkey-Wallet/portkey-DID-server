cc4656ec81d31914d2388420d0577aa5be923bbd
Edward-BXS <Magic.Wizard@portkey.finance>
2024-07-09 22:10:00 +0800

    comment signature server code

--- src/CAServer.Signature/Provider/SignatureProvider.cs
+++ src/CAServer.Signature/Provider/SignatureProvider.cs
@@ -44,27 +44,28 @@ public class SignatureProvider : ISignatureProvider, ISingletonDependency
             PublicKey = publicKey,
             HexMsg = hexMsg,
         };
-
-        var resp = await _httpProvider.InvokeAsync<CommonResponseDto<SignResponseDto>>(HttpMethod.Post,
-            Uri(GetSecurityUri), 
-            body: JsonConvert.SerializeObject(signatureSend),
-            header: SecurityServerHeader()
-            );
         //todo cancel comments before online, the signature server didnt' deploy in test1 env
+        // var resp = await _httpProvider.InvokeAsync<CommonResponseDto<SignResponseDto>>(HttpMethod.Post,
+        //     Uri(GetSecurityUri), 
+        //     body: JsonConvert.SerializeObject(signatureSend),
+        //     header: SecurityServerHeader()
+        //     );
+        
         // AssertHelper.IsTrue(resp?.Success ?? false, "Signature response failed");
         // AssertHelper.NotEmpty(resp!.Data?.Signature, "Signature response empty");
-        return resp.Data!.Signature;
+        // return resp.Data!.Signature;
+        return "";
     }
     
-    public Dictionary<string, string> SecurityServerHeader(params string[] signValues)
-    {
-        var signString = string.Join(CommonConstant.EmptyString, signValues);
-        return new Dictionary<string, string>
-        {
-            ["appid"] = _signatureServerOptions.CurrentValue.AppId,
-            ["signature"] = EncryptionHelper.EncryptHex(signString, _signatureServerOptions.CurrentValue.AppSecret)
-        };
-    }
+    // public Dictionary<string, string> SecurityServerHeader(params string[] signValues)
+    // {
+    //     var signString = string.Join(CommonConstant.EmptyString, signValues);
+    //     return new Dictionary<string, string>
+    //     {
+    //         ["appid"] = _signatureServerOptions.CurrentValue.AppId,
+    //         ["signature"] = EncryptionHelper.EncryptHex(signString, _signatureServerOptions.CurrentValue.AppSecret)
+    //     };
+    // }
 }
 
 public class SendSignatureDto
