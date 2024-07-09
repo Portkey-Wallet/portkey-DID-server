66f29cf4311a7f9eacc7f1ee2525fdab99b5bd40
Edward-BXS <Magic.Wizard@portkey.finance>
2024-07-09 21:03:04 +0800

    change period

--- src/CAServer.EntityEventHandler.Core/Worker/WorkerConst.cs
+++ src/CAServer.EntityEventHandler.Core/Worker/WorkerConst.cs
@@ -7,7 +7,7 @@ public class WorkerConst
     public const int TimePeriod = 3000;
     public const int MaxOlderBlockHeightFromNow = 100000;
     public const int CryptoGiftExpiredTimePeriod = 60000;
-    public const int CryptoGiftStatisticsPeriod = 60000;
+    public const int CryptoGiftStatisticsPeriod = 2160000;
     public const int InitReferralTimePeriod = 30000;
     public const int ReferralTimePeriod = 20000;
 }
