9b50b03c3d0035525567df58fb6d64237562618c
felix-zhaolei <felix@portkey.finance>
2024-07-19 13:46:47 +0800

    modify: free mint eto.

--- src/CAServer.Application.Contracts/FreeMint/Etos/FreeMintEto.cs
+++ src/CAServer.Application.Contracts/FreeMint/Etos/FreeMintEto.cs
@@ -1,8 +1,10 @@
 using System;
 using CAServer.FreeMint.Dtos;
+using Volo.Abp.EventBus;
 
 namespace CAServer.FreeMint.Etos;
 
+[EventName("FreeMintEto")]
 public class FreeMintEto
 {
     public Guid UserId { get; set; }
--- src/CAServer.ContractEventHandler.Core/FreeMintEventHandler.cs
+++ src/CAServer.ContractEventHandler.Core/FreeMintEventHandler.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using CAServer.ContractEventHandler.Core.Application;
 using CAServer.FreeMint.Etos;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.EventBus.Distributed;
 
@@ -9,14 +11,17 @@ namespace CAServer.ContractEventHandler.Core;
 public class FreeMintEventHandler : IDistributedEventHandler<FreeMintEto>, ITransientDependency
 {
     private readonly IMintNftItemService _mintNftItemService;
+    private readonly ILogger<FreeMintEventHandler> _logger;
 
-    public FreeMintEventHandler(IMintNftItemService mintNftItemService)
+    public FreeMintEventHandler(IMintNftItemService mintNftItemService, ILogger<FreeMintEventHandler> logger)
     {
         _mintNftItemService = mintNftItemService;
+        _logger = logger;
     }
 
     public async Task HandleEventAsync(FreeMintEto eventData)
     {
+        _logger.LogInformation("Begin Handle FreeMint: data:{data}", JsonConvert.SerializeObject(eventData));
         _ = _mintNftItemService.MintAsync(eventData);
     }
 }
