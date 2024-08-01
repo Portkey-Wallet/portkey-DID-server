7db63612c7ab0b3e02f0bc3912481af60ff7df09
wangyue <sean@portkey.finance>
2024-08-01 20:04:29 +0800

    feat:fix hamster bug

--- src/CAServer.Application/Growth/GrowthStatisticAppService.cs
+++ src/CAServer.Application/Growth/GrowthStatisticAppService.cs
@@ -479,14 +479,7 @@ public class GrowthStatisticAppService : CAServerAppService, IGrowthStatisticApp
                     ReferralAddress = hamsterReferralDic[referralCaHash].ReferralAddress,
                     ReferralType = 1
                 };
-                var addResult = await _growthProvider.AddReferralRecordAsync(index);
-                if (!addResult)
-                {
-                    continue;
-                }
-
-                var expired = TimeSpan.FromDays(_hamsterOptions.HamsterExpired);
-                await _cacheProvider.SetAddAsync(OverHamsterScoreLimitKey, hamster.CaAddress, expired);
+                await _growthProvider.AddReferralRecordAsync(index);
             }
         }
     }
@@ -506,12 +499,6 @@ public class GrowthStatisticAppService : CAServerAppService, IGrowthStatisticApp
             }
 
             var formatAddress = _hamsterOptions.AddressPrefix + address + _hamsterOptions.AddressSuffix;
-            var overLimitAddress = await _cacheProvider.SetMembersAsync(OverHamsterScoreLimitKey);
-            if (overLimitAddress.Contains(formatAddress))
-            {
-                continue;
-            }
-
             addresses.Add(formatAddress);
             userInfoDic.Add(address, holderInfo.CaHolderInfo.FirstOrDefault()?.CaHash);
         }
