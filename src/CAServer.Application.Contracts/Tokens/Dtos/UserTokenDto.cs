46cd5dcb42e9f9cf649eb3f896a7269fad7070bb
sunny <sunny.k@portkey.finance>
2024-10-30 18:46:51 +0800

    add chain display name, fix display

--- src/CAServer.Application.Contracts/Tokens/Dtos/UserTokenDto.cs
+++ src/CAServer.Application.Contracts/Tokens/Dtos/UserTokenDto.cs
@@ -13,9 +13,10 @@ public class UserTokenDto
     public Token Token { get; set; }
 }
 
-public class Token : ChainDisplayNameDto
+public class Token
 {
     public Guid Id { get; set; }
+    public string ChainId { get; set; }
     public string Address { get; set; }
     public string Symbol { get; set; }
     public int Decimals { get; set; }
