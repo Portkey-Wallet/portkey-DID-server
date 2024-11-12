48d276e29f10f9289461750ece7d9ecdb65d7e76
felix-zhaolei <felix@portkey.finance>
2024-11-12 09:34:18 +0800

    modify: address book add error code.

--- src/CAServer.Grains/Grain/AddressBook/AddressBookMessage.cs
+++ src/CAServer.Grains/Grain/AddressBook/AddressBookMessage.cs
@@ -5,4 +5,6 @@ public class AddressBookMessage
     public const string NotExistMessage = "Contact not exist.";
     public const string ExistedMessage = "This name already exists";
     public const string HolderNullMessage = "CaHolder can not be null";
+    public const string NameExistedCode = "40021";
+    public const string AddressInvalidCode = "40022";
 }
