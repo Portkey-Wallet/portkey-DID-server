d127ebeb33bb0335efcc02693c818bccd99fd38d
sunny <sunny.k@portkey.finance>
2024-11-29 11:21:00 +0800

    add websiteValid method

--- src/CAServer.Application.Contracts/Commons/WebsiteInfoHelper.cs
+++ src/CAServer.Application.Contracts/Commons/WebsiteInfoHelper.cs
@@ -11,7 +11,7 @@ public class WebsiteInfoHelper
         {
             if (website.logo.Equals(info.logo))
             {
-                return website.website.Equals(info.website);
+                return website.website.Contains(info.website);
             }
         }
 
@@ -21,14 +21,15 @@ public class WebsiteInfoHelper
 
     private static List<WebsiteInfoDto> WebsiteInfoes = new List<WebsiteInfoDto>
     {
-        new WebsiteInfoDto { website = "https://app.etransfer.exchange", logo = "https://icon.horse/icon/app.etransfer.exchange/50" },
-        new WebsiteInfoDto { website = "https://ebridge.exchange", logo = "https://icon.horse/icon/ebridge.exchange/50" },
-        new WebsiteInfoDto { website = "https://app.awaken.finance", logo = "https://icon.horse/icon/app.awaken.finance/50" },
-        new WebsiteInfoDto { website = "https://aefinder.io", logo = "https://icon.horse/icon/aefinder.io/50" },
-        new WebsiteInfoDto { website = "https://cat.schrodingernft.ai", logo = "https://icon.horse/icon/cat.schrodingernft.ai/50" },
-        new WebsiteInfoDto { website = "https://www.eforest.finance", logo = "https://icon.horse/icon/www.eforest.finance/50" },
-        new WebsiteInfoDto { website = "https://pixiepoints.io", logo = "https://icon.horse/icon/pixiepoints.io/50" },
-        new WebsiteInfoDto { website = "https://tmrwdao.com", logo = "https://icon.horse/icon/tmrwdao.com/50" },
-        new WebsiteInfoDto { website = "https://ewell.finance", logo = "https://icon.horse/icon/ewell.finance/50" },
+        new WebsiteInfoDto { website = "etransfer.exchange", logo = "https://icon.horse/icon/app.etransfer.exchange/50" },
+        new WebsiteInfoDto { website = "ebridge.exchange", logo = "https://icon.horse/icon/ebridge.exchange/50" },
+        new WebsiteInfoDto { website = "awaken.finance", logo = "https://icon.horse/icon/app.awaken.finance/50" },
+        new WebsiteInfoDto { website = "aefinder.io", logo = "https://icon.horse/icon/aefinder.io/50" },
+        new WebsiteInfoDto { website = "schrodingernft.ai", logo = "https://icon.horse/icon/cat.schrodingernft.ai/50" },
+        new WebsiteInfoDto { website = "eforest.finance", logo = "https://icon.horse/icon/www.eforest.finance/50" },
+        new WebsiteInfoDto { website = "pixiepoints.io", logo = "https://icon.horse/icon/pixiepoints.io/50" },
+        new WebsiteInfoDto { website = "tmrwdao.com", logo = "https://icon.horse/icon/tmrwdao.com/50" },
+        new WebsiteInfoDto { website = "ewell.finance", logo = "https://icon.horse/icon/ewell.finance/50" },
+        new WebsiteInfoDto { website = "hamster.beangotown", logo = "https://icon.horse/icon/hamster.beangotown.com/50" },
     };
 }
