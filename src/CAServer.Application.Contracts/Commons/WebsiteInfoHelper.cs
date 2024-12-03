f95113bc4a6b8c716d5f45c66c8d8bd52cec23fd
sunny <sunny.k@portkey.finance>
2024-12-03 11:44:12 +0800

    dapp safe, add hamster

--- src/CAServer.Application.Contracts/Commons/WebsiteInfoHelper.cs
+++ src/CAServer.Application.Contracts/Commons/WebsiteInfoHelper.cs
@@ -9,10 +9,15 @@ public class WebsiteInfoHelper
     {
         foreach (var website in WebsiteInfoes)
         {
-            if (website.logo.Equals(info.logo))
+            if (info.website.Replace("https://","").Equals(website.website))
             {
-                return info.website.Contains(website.website);
+                return info.logo.Equals(website.logo);
             }
+            if (info.website.Contains(website.website))
+            {
+                return info.logo.Contains(info.website);
+            }
+            
         }
 
         return true;
@@ -23,6 +28,7 @@ public class WebsiteInfoHelper
     {
         new WebsiteInfoDto { website = "etransfer.exchange", logo = "https://icon.horse/icon/app.etransfer.exchange/50" },
         new WebsiteInfoDto { website = "ebridge.exchange", logo = "https://icon.horse/icon/ebridge.exchange/50" },
+        new WebsiteInfoDto { website = "test.ebridge.exchange", logo = "https://icon.horse/icon/test.ebridge.exchange/50" },
         new WebsiteInfoDto { website = "awaken.finance", logo = "https://icon.horse/icon/app.awaken.finance/50" },
         new WebsiteInfoDto { website = "aefinder.io", logo = "https://icon.horse/icon/aefinder.io/50" },
         new WebsiteInfoDto { website = "schrodingernft.ai", logo = "https://icon.horse/icon/cat.schrodingernft.ai/50" },
@@ -31,6 +37,5 @@ public class WebsiteInfoHelper
         new WebsiteInfoDto { website = "tmrwdao.com", logo = "https://icon.horse/icon/tmrwdao.com/50" },
         new WebsiteInfoDto { website = "ewell.finance", logo = "https://icon.horse/icon/ewell.finance/50" },
         new WebsiteInfoDto { website = "hamster.beangotown", logo = "https://icon.horse/icon/hamster.beangotown.com/50" },
-        new WebsiteInfoDto { website = "hamster.beangotown", logo = "https://icon.horse/icon/hamster.beangotown.xyz/50" },
     };
 }
