5c4d8ccd723e10325bbc4a87d2b1e668d9658f6b
lip-w <lip@portkey.finance>
2025-11-03 11:35:26 +0800

    chore: disable MongoDB log output in unit test workflow

--- test/CAServer.MongoDB.Tests/MongoDb/CAServerMongoDbFixture.cs
+++ test/CAServer.MongoDB.Tests/MongoDb/CAServerMongoDbFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Extensions.Logging.Abstractions;
 using Mongo2Go;
 
 namespace CAServer.MongoDB;
@@ -10,7 +11,7 @@ public class CAServerMongoDbFixture : IDisposable
     
     static CAServerMongoDbFixture()
     {
-        MongoDbRunner = MongoDbRunner.Start(singleNodeReplSet: true, singleNodeReplSetWaitTimeout: 10);
+        MongoDbRunner = MongoDbRunner.Start(singleNodeReplSet: true, singleNodeReplSetWaitTimeout: 10, logger: NullLogger.Instance);
         ConnectionString = MongoDbRunner.ConnectionString;
     }
 
